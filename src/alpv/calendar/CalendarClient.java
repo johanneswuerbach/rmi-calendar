f2ccbcdb43e745682f8f7fce63f969b6c0ec017c
Johannes Würbach <johannes.wuerbach@googlemail.com>
2011-12-07 15:35:14 +0100

    Made client serializable

--- src/alpv/calendar/CalendarClient.java
+++ src/alpv/calendar/CalendarClient.java
@@ -3,6 +3,7 @@ package alpv.calendar;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.Serializable;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
@@ -12,8 +13,9 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.List;
 
-public class CalendarClient implements EventCallback {
+public class CalendarClient implements EventCallback, Serializable {
 
+	private static final long serialVersionUID = 6395630201109375324L;
 	CalendarServer _calendarServer;
 
 	public CalendarClient(String host, int port) throws RemoteException,
